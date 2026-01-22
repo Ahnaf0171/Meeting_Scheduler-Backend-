cc798f8563298e44a77c4604cd49f051a56a62d4
Abdullah Ahnaf <abdullahahnaf4567@gmail.com>
2026-01-22 13:56:53 +0600

    urls updated

--- meeting_scheduler/urls.py
+++ meeting_scheduler/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "",
+        "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
